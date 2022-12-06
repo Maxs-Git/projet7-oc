9e8013756495f7617d82638c93af0e247f35f5cd
max <maxetness@gmail.com>
2022-12-06 11:23:45 +0100

    suppression .env

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -29,12 +29,6 @@ exports.getAllPost = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-// exports.getOnePost = (req, res, next) => {
-//   Post.findOne({ _id: req.params.id })
-//     .then((post) => res.status(200).json(post))
-//     .catch((error) => res.status(404).json({ error }));
-// };
-
 exports.modifyPost = (req, res, next) => {
   const postObject = req.file
     ? {
